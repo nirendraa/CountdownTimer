fd49ab187c68b3c7118d234881b239dfc4d51fed
Nirendra <nirendra@gmail.com>
2020-01-05 05:46:12 +0530

    Test

---
+++ app/src/main/java/com/example/timerdemo/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.timerdemo;
+
+import android.os.CountDownTimer;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    public int counter;
+    Button button;
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button= (Button) findViewById(R.id.button);
+        textView= (TextView) findViewById(R.id.textView);
+        button.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v) {
+                new CountDownTimer(30000, 1000){
+                    public void onTick(long millisUntilFinished){
+                        textView.setText(String.valueOf(counter));
+                        counter++;
+                    }
+                    public  void onFinish(){
+                        textView.setText("FINISH!!");
+                    }
+                }.start();
+            }
+        });
+    }
+}
